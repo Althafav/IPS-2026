b2e287cf0eb5441f84a5e283199804b0274acc68
Althaf Muhammed Umer <61247025+althafMuhammedUmer@users.noreply.github.com>
2025-08-15 09:07:06 +0400

    removed unwanted script

--- components/Layout/FooterComponent.tsx
+++ components/Layout/FooterComponent.tsx
@@ -46,35 +46,7 @@ export default function FooterComponent() {
     <div className="relative ">
       <SlantedSection className="bg-[#333333] absolute -bottom-80 " />
       <footer className="footer-component-wrapper ">
-        <svg
-          className="animated-streaming-module__PB4uha__pulse pulse"
-          data-position="top-left"
-          fill="none"
-          height="51"
-          viewBox="0 0 237 51"
-          width="237"
-        >
-          <path
-            stroke="url(#_R_92cjaubaaivb_)"
-            stroke-linecap="round"
-            stroke-width="1.881606931303395"
-            d="M0.5 1.5L43 1.5C43.5523 1.5 44 1.94772 44 2.5L44 48.5C44 49.0523 44.4477 49.5 45 49.5L91 49.5C91.5523 49.5 92 49.0523 92 48.5L92 41C92 36.8579 95.3579 33.5 99.5 33.5L236 33.5"
-          ></path>
-          <defs>
-            <linearGradient
-              gradientUnits="userSpaceOnUse"
-              id="_R_92cjaubaaivb_"
-              x1="161.84300534951035"
-              x2="87.77079369698185"
-              y1="0"
-              y2="-1.6687618909054436"
-            >
-              <stop stop-color="#3291FF" stop-opacity="0"></stop>
-              <stop stop-color="#3291FF"></stop>
-              <stop offset="1" stop-color="#61DAFB" stop-opacity="0"></stop>
-            </linearGradient>
-          </defs>
-        </svg>
+       
         <div className="container mx-auto   ">
           <div className="py-10">
             <form
